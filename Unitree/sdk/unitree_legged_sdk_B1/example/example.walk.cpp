b02ba07b38d54711edf80b9c515dd6ebcf50a2b3
bongoos <110041653+bongoos@users.noreply.github.com>
2024-06-12 09:51:39 +0900

    Update example.walk.cpp

--- Unitree/sdk/unitree_legged_sdk_B1/example/example.walk.cpp
+++ Unitree/sdk/unitree_legged_sdk_B1/example/example.walk.cpp
@@ -1,1 +1,13 @@
-good
+/****
+Copyright (c) 2020, Unitree Robotics.Co.Ltd.All right reserved.
+****/
+
+/****
+Code analysis by bon, June 12, 2024
+****/
+
+#include "unitree_legged_sdk/unitree_legged.sdk.h" #unitree.legged.sdk 헤더 파일 포함하고 있으므로 어떤 헤더파일인지 확인 필요
+#include <math.h>
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
