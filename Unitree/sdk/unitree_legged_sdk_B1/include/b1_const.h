332abe316980641d21f8668ebf8fdfa126bf1e43
bongoos <110041653+bongoos@users.noreply.github.com>
2024-06-17 16:34:28 +0900

    Update b1_const.h

--- Unitree/sdk/unitree_legged_sdk_B1/include/b1_const.h
+++ Unitree/sdk/unitree_legged_sdk_B1/include/b1_const.h
@@ -11,14 +11,14 @@ Header anlaysis by bon June 17, 2024
 namespace UNITREE_LEGGED_SDK
 {
   //엉덩이 관절 최대/최소 각도[radian]
-  constexpr double b1_Hip_max = 0.75;
-  constexpr double b1_Hip_min = -0.75;
+  constexpr double b1_Hip_max = 0.75; //42.97도
+  constexpr double b1_Hip_min = -0.75; //-42.97도
   //허벅지 관절 최대/최소 각도[radian]
-  constexpr double b1_Thigh_max = 3.5;
-  constexpr double b1_Thigh_min = -1.0;
+  constexpr double b1_Thigh_max = 3.5; //200.54도
+  constexpr double b1_Thigh_min = -1.0; //-57.3도
   //종아리 관절 최대/최소 각도[radian]
-  constexpr double b1_Calf_max = -0.6;
-  constexpr double b1_Calf_min = -2.6;
+  constexpr double b1_Calf_max = -0.6; //-34.38도
+  constexpr double b1_Calf_min = -2.6; //-148.97도
 }
 #endif
 
