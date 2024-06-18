8ae376f38ed195ab9c08788e21dc44a2a0a44c9f
bongoos <110041653+bongoos@users.noreply.github.com>
2024-06-18 13:36:35 +0900

    Update safety.h

--- Unitree/sdk/unitree_legged_sdk_B1/include/safety.h
+++ Unitree/sdk/unitree_legged_sdk_B1/include/safety.h
@@ -4,6 +4,7 @@ Copyright(c) 2020, Unitree Robotics.Co.Ltd. All right reserved.
 
 /**
 Header analysis by bon, June 17, 2024
+**/
 
 #ifndef _UNITREE_LEGGED_SAFETY_H_
 #define _UNITREE_LEGGED_SAFETY_H_
