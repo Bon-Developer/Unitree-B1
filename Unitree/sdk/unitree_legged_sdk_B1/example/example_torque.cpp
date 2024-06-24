6c24c9684b93a11c6e66173c23b6e880f82ddda0
bongoos <110041653+bongoos@users.noreply.github.com>
2024-06-24 13:33:34 +0900

    Update example_torque.cpp

--- Unitree/sdk/unitree_legged_sdk_B1/example/example_torque.cpp
+++ Unitree/sdk/unitree_legged_sdk_B1/example/example_torque.cpp
@@ -51,7 +51,37 @@ void Custom::RobotControl()
 {
   motiontime++; //제어 루프의 진행 추적
   udp.GetRecv(state); //현재 로봇의 상태 수신
-  printf("%d %f %f\n", motiontime, state.motorState[FR_1].q, state.motorState[FR_1].dq); //첫 번째 관절의 각도와 각속도 출력
+  printf("%d %f %f\n", motiontime, state.motorState[FR_1].q, state.motorState[FR_1].dq); //FR_1 관절의 각도와 각속도 출력
+
+  //gravity compensation
+  cmd.motorCmd[FR_0].tau = -5.0f; //FR_0 관절에 중력 보상 토크 적용
+  //cmd.motorCmd[FL_0].tau = +0.65f;
+  //cmd.motorCmd[RR_0].tau = -0.65f;
+  //cmd.motorCmd[RL_0].tau = +0.65f;
+
+  //float freq_Hz = 1;
+  //float freq_Hz = 2;
+  //float freq_Hz = 5;
+  //float freq_rad = freq_Hz * 2 * M_PI;
+  //float t = dt * sin_count;
+
+  if(motiontime >= 500)
+  {
+    sin_count++; //제어 주기
+    float torque = (0 - state.motorState[FR_1].q) * 10.0f + (0 - state.motorState[FR_1].dq) * 1.0f; //관절 현재 각도와 각속도를 이용하여 토크 계산
+    //float torque = (0 - state.motorState[FR_1].q) * 20.0f + (0 - state.motorState[FR_1].dq) * 2.0f;
+    //float torque = (0 - state.motorState[FR_1].q) * 40.0f + (0 - state.motorState[FR_1].dq) * 2.0f;
+    //float torque = 2 * sin(t * freq_rad);
+
+    //계산된 토크가 일정 범위를 넘지 않도록 클리핑
+    if(torque > 5.0f) torque = 5.0f;
+    if(torque < -5.0f) torque = -5.0f;
+    //if(torque > 15.0f) torque = 15.0f;
+    //if(torque < -15.0f) torque = -15.0f;
+
+    cmd.motorCmd[FR_1].q = PosStopF;
+    
+
 
 
 
