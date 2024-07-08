09b865c1c5239faeb287428517737b93fbdeacb1
bongoos <110041653+bongoos@users.noreply.github.com>
2024-07-08 11:40:25 +0900

    Update base.cpp

--- ws_qre_b1/src/b1/b1_base/src/base.cpp
+++ ws_qre_b1/src/b1/b1_base/src/base.cpp
@@ -1,4 +1,13 @@
 # include "b1_base/base.hpp"
 
 using namespace qre;
-Base::Base(UT::HighCmd cmd, U
+Base::Base(UT::HighCmd cmd, UT::HighState state, ros::NodeHandle *nh, ros::NodeHandle *p_nh): robot_safety_(UT::LeggedType::B1)
+  nh_ = nh;
+  robot_high_cmd.mode            = 2;
+  robot_high_cmd.gaitType        = 1;
+  robot_high_cmd.velocity        = {0.0};
+  robot_high_cmd.position        = {0.0};
+  robot_high_cmd.yawSpeed        = 0.0;
+  robot_high_cmd.euler           = {0.0};
+  robot_high_cmd.bodyHeight      = 0.0;
+  robot_high_cmd.footRaiseHeight = 0.0;
