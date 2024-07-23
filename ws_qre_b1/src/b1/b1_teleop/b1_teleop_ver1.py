929249abf359b7a4a675613971ebe2198cab5d78
bongoos <110041653+bongoos@users.noreply.github.com>
2024-07-23 17:25:03 +0900

    Update b1_teleop_ver1.py

--- ws_qre_b1/src/b1/b1_teleop/b1_teleop_ver1.py
+++ ws_qre_b1/src/b1/b1_teleop/b1_teleop_ver1.py
@@ -1,4 +1,4 @@
-# 240723 code develop: 키보드 조종에 따른 swing height 변경 추가
+# 240723(Park SangTae): SetMode, SetBodyHeight, SetFootRiaseHeight 추가
 
 #!/user/bin/env python
 #credits to: http://github.com/ros-teleop/teleop_twist_keyboard/blob/master/teleop_twist_keyboard.py
@@ -11,7 +11,10 @@ import rospy #rospy 라이브러리 가져오기, ROS 노드 초기화 및 ROS �
 
 from geometry_msgs.msg import Twist #로봇 이동 명령(방향 및 속도)
 from champ_msgs.msg import Pose as PoseLite #로봇 자세 데이터
-from geometry_msgs.msg import Pose as Pose #3차원 공간에서의 위치 방향 
+from geometry_msgs.msg import Pose as Pose #3차원 공간에서의 위치 방향
+
+#240723
+from b1_legged_msgs.msg import SetMode, SetBodyHeight, SetFootRaiseHeight
 import tf #ROS에서 좌표 변환 처리하는 라이브러리
 
 import sys, select, termios, tty
@@ -20,14 +23,36 @@ import numpy as np
 class Teleop:
     def __init__(self):
       self.velocity_publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1) #토픽: cmd_vel, 메시지 타입: Twist
+
+      #base.hpp에 'body_pose/raw', 'body_pose'를 받는 subscriber가 정의되어있지 않음
       self.pose_lite_publisher = rospy.Publisher('body_pose/raw', PoseLite, queue_size = 1) #토픽: body_pose/raw, 메시지 타입: PoseLite
       self.pose_publisher = rospy.Publisher('body_pose', Pose, queue_size = 1) #토픽: body_pose, 메시지 타입: Pose
+
+      #240723    
+      # ================ Custom publisher ================
+      # base.hpp에서 ros::ServiceServer set_mode_에게 servie를 전송할 client -> mode와 gait type 변경 가능
+      self.mode_publisher = rospy.Publisher("/b1_controller/set_mode", SetMode, queue_size = 1)
+      self.body_height_publisher = rospy.Publisher("/b1_controller/set_body_height", SetBodyHeight, queue_size = 1)
+      self.foot_raise_height_publisher = rospy.Publisher("/b1_controller/set_foot_raise_height", SetFootRaiseHeight, queue_size = 1)
+      # ==================================================
+
+      # ================ Custom parameter ================
+      self.body_height = rospy.get_param("~body_height", 0.5)
+      self.foot_raise_height = rospy.get_param("~foot_raise_height", 0)
+      self.gait_type = rospy.get_param("~gait_type", 1)
+      # ==================================================
+
+      #Parameters
       self.swing_height = rospy.get_param("gait/swing_height", 0) #파라미터 이름: gait/swing_hieght, 파라미터 값 가져옴. 기본값 0
       self.nominal_height = rospy.get_param("gait/nominal_height", 0) #파라미터 이름: gait/nominal_height, 파라미터 값 가져옴. 기본값 0
 
       self.speed = rospy.get_param("~speed", 0.1) #기본 속도 0.1m/s
       self.turn = rospy.get_param("~turn", 0.1) #기본 속도 0.1m/s
 
+
+
+
+        
       
       self.msg = """
       Reading from the keyboard and Publishing to Twist!
@@ -47,6 +72,9 @@ class Teleop:
       q/z : increase/decrease max speeds by 10%
       w/x : increase/decrease only linear speed by 10%
       e/c : increase/decrease only angular speed by 10%
+      a/s : increase/decrease body height +- 0.01
+      d/f : increase/decrease foot raise height +- 0.01
+      y : mode change
       CTRL-C to quit
                 """
 
@@ -87,6 +115,12 @@ class Teleop:
                 'x':(-0.1,0),
                 'e':(0,0.1),
                 'c':(0,-0.1),
+            }
+        self.customBindings = {
+                'a':(0.01, 0), #bodyHeight + 0.01
+                's':(-0.01, 0),
+                'd':(0, 0.01), #footRaiseHeight + 0.01
+                'f':(0, -0,01)
             }
         
       self.poll_keys()
@@ -146,8 +180,54 @@ class Teleop:
                         print(self.msg)
                     status = (status + 1) % 15
 
-                elif key in self.poseBindings.keys():
-                    self.swing_height
+                elif key in self.customBindings.keys():
+                    if key in ['a', 's']:
+                        req_body = SetBodyHeight()
+                        req_body.value = self.customBindings[key][0]
+                        print(req_body.value)
+                        self.body_height_publisher.publish(req_body)
+
+                    elif key in ['d', 'f']:
+                        req_foot = SetFootRaiseHeight()
+                        req_foot.value = self.customBindings[key][1]
+                        print(req_foot.value)
+                        self.foot_raise_height_publisher.publish(req_foot)
+
+                elif key == 'y':
+                    self.mode_info = '''
+                    # 0. idle, default stand
+                    # 1. force stand
+                    # 2. target velocity walking
+                    # 3. target position walking
+                    # 4. path mode walking
+                    # 5. position stand down
+                    # 6. position stand up
+                    # 7. damping mode
+                    # 8. recovery stand
+                    # 9. backflip
+                    # 10. jumpYaw 
+                    # 11. straightHand
+                    # 12. dance1
+                    # 13. dance2
+                    '''
+
+                    print(self.mode_info)
+                    self.mode = int(input("Select mode: "))
+
+                    self.gait_type_info = "0. idle, 1. trot, 2. trot running, 3. climb start, 4. trot obstacle"
+                    print(self.gait_type_info)
+                    self.gait_type = int(input("Select gait type : ))
+
+                    if self.gait_type < 0 or self.gait_type > 4:
+                       raise ValueError("Invalid gait type. Please select a number between 0 and 4.")
+                    elif self.mode < 0 or self.mode > 14:
+                       raise ValueError("Invalid mode. Please select a number between 0 and 13.")
+
+                    req_mode = SetMode()
+                    req_mode.mode = self.mode
+                    req_mode.speed_level = 1
+                    req_mode.gait_type = self.gait_type
+                    self.mode_publisher.publish(req_mode)
 
                 else:
                     cmd_attempts = 0 #아무 명령이 없으면 0으로 초기화
