f8e87132a835e7f4f7bc8a199b0b6268137b87f2
bongoos <110041653+bongoos@users.noreply.github.com>
2024-07-23 14:08:27 +0900

    Create base_ver1.hpp

---
+++ ws_qre_b1/src/b1/b1_base/include/b1_base/base_ver1.hpp
@@ -0,0 +1,78 @@
+// Analysis by bon, July 16, 2024
+// 240723(Park SangTae): mode_subscriber_, body_height_subscriber_, foot_raise_height_subscriber 추가
+
+#ifndef _BASE_H
+#define _BASE_H 
+
+#include <math.h>
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <geometry_msgs/Twist.h>
+#include <tf/transform_broadcaster.h> //분석 필요
+#include <sensor_msgs/Imu.h> //분석 필요
+#include "unitree_legged_sdk/unitree_legged_sdk.h" //분석 필요
+#include "legacy_wrapper/quadruped.hpp" //분석 필요
+#include "b1_legged_msgs/SetMode.h" //분석 필요
+#include "b1_legged_msgs/SetControl.h" //분석 필요
+#include "b1_legged_msgs/JointCmd.h" //분석 필요
+
+//240723
+#include "b1_legged_msgs/SetBodyHeight.h"
+#include "b1_legged_msgs/SetFootRaiseHeight.h"
+
+namespace qre{
+  void heartbeatMonitor();
+  class Base : public Quadruped {
+    ros::NodeHandle *nh_, *p_nh_;
+
+    //서브스크라이버
+    ros::Subscriber cmd_subscriber_;
+
+    //퍼블리셔
+    ros::Publisher joint_state_publisher_;
+    ros::Publisher state_publisher_;
+    ros::Publisher imu_publiser_;
+    ros::Publisher battery_state_publisher_;
+    ros::Publisher motor_state_publisher_;
+    // tf::TransformBroadcaster odom_broadcaster_;
+
+    //서비스서버
+    //ros::ServiceServer set_mode_;
+    ros::ServiceServer set_control_;
+
+
+    //240723
+    ros::Subscriber mode_subscriber_;
+    ros::Subscriber body_height_subscirber_;
+    ros::Subscriber foot_raise_height_subscirber_;
+
+    UT::Safety robot_safety_;
+    UT::UDP* comm_bus_;
+    uint16_t level_;
+    std::string modes_[4] = {"Position", "Velocity", "Torque", "Full"}; //제어 모드 설정
+    std::string low_level_control_type_ = "Position";
+    char udp_ip_[16];
+
+  public:
+    float dt = 0.002; //0.001 ~ 0.01
+    Base(UT::Highcmd cmd, UT::HighState state, ros::NodeHandle *nh, ros::NodeHandle *p_nh);
+    Base(UT::LowCmd cmd, UT::LowState state, ros::NodeHandle *nh, ros::NodeHandle *p_nh);
+    void HighLevelControl();
+    void LowLevelControl();
+    void packetReceive();
+    void packetSend();
+    void cmdVelCallback(geometry_msgs::Twist msg);
+    void jointCommandCallback(b1_legged_msgs::JointCmd msg);
+    void publishStateMessages();
+    //bool setModeCallback(b1_legged_msgs::SetMode::Request &req, b1_legged_msgs::SetMode::Response &res);
+    bool setControlCallback(b1_legged_msgs::SetControl::Request &req, b1_legged_msgs::SetControl::Response &res);
+
+    //240723
+    void modeCallback(const b1_legged_msgs::SetMode::ConstPtr& msg);
+    void bodyHeightCallback(const b1_legged_msgs::SetBodyHeight::ConstPtr& msg);
+    void footRaiseHeightCallback(b1_legged_msgs::SetFootRaiseHeight msg);
+  };
+}
+
+#endif
